c84118b4c25b4430bd251d78d05e4eae893c647a
mathav-ramalingam <mathavramalingam1608@gmail.com>
2025-05-20 01:12:10 +0530

    UI updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,40 +1,38 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './Navbar.jsx';
-import Signin from './login/Signin.jsx';
-import Dasboard from './dashboard/Dasboard.jsx';
-import Doctordashboard from './dashboard/Doctordashboard.jsx';
-import Signup from './login/Signup.jsx';
-import Admindashboard from './dashboard/Admindashboard.jsx';
-import AdminDoctor from './dashboard/AdminDoctor.jsx';
-import AdminHospital from './dashboard/AdminHospital.jsx';
-import AdminVaccine from './dashboard/AdminVaccine.jsx';
-import AdminHosp from './dashboard/AdminHosp.jsx';
-import General from './components/General.jsx';
-import DiseaseDetail from './components/diseaseDetail.jsx';
+import React from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./Navbar.jsx";
+import Signin from "./login/Signin.jsx";
+import Dasboard from "./dashboard/Dasboard.jsx";
+import Doctordashboard from "./dashboard/Doctordashboard.jsx";
+import Signup from "./login/Signup.jsx";
+import Admindashboard from "./dashboard/Admindashboard.jsx";
+import AdminDoctor from "./dashboard/AdminDoctor.jsx";
+import AdminHospital from "./dashboard/AdminHospital.jsx";
+import AdminVaccine from "./dashboard/AdminVaccine.jsx";
+import AdminHosp from "./dashboard/AdminHosp.jsx";
+import General from "./components/General.jsx";
+import DiseaseDetail from "./components/diseaseDetail.jsx";
 
 function Layout() {
   const location = useLocation();
-  
+
   return (
     <div>
       {/* Show Navbar only on Dashboard (/) */}
-      {location.pathname === '/' && <Navbar />}
-      
+      {location.pathname === "/" && <Navbar />}
+
       <Routes>
-        
         <Route path="/" element={<Dasboard />} />
         <Route path="/login" element={<Signin />} />
-        <Route path='/general' element={<General />} />
+        <Route path="/general" element={<General />} />
         <Route path="/general/:id" element={<DiseaseDetail />} />
         <Route path="/doctor-dashboard" element={<Doctordashboard />} />
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/admin-nav" element={<Admindashboard/>}/>
-        <Route path="/admin" element={<AdminDoctor/>} />
-        <Route path="/adminhospital" element={<AdminHospital/>}/>
-        <Route path="/adminvaccine" element={<AdminVaccine/>}/>
-        <Route path='/adminhosp' element={<AdminHosp/>}/>
-
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/admin-nav" element={<Admindashboard />} />
+        <Route path="/admin" element={<AdminDoctor />} />
+        <Route path="/adminhospital" element={<AdminHospital />} />
+        <Route path="/adminvaccine" element={<AdminVaccine />} />
+        <Route path="/adminhosp" element={<AdminHosp />} />
       </Routes>
     </div>
   );
